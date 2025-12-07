1301893706c58fe59471a29b41f625f693f19d9e
YamaguchiKoki <shankouhanghui15@gmail.com>
2025-12-08 00:44:05 +0900

    feat: add policy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,48 @@
+import { Highlighter } from "@/components/ui/highlighter";
+import { PageTitle } from "@/feature/post/components/PageTitle";
+
 export default async function Home() {
   return (
     <main className="max-w-4xl py-20 px-16">
-      <h1 className="text-3xl font-bold mb-8">このサイトについて</h1>
-      <p>WIP</p>
+      <PageTitle title="このサイトについて" subtitle />
+      <div className="flex flex-col gap-y-4">
+        <section>
+          <h2 className="text-2xl font-bold mb-4">
+            <Highlighter action="underline" color="#0000ff" multiline={false}>
+              やること
+            </Highlighter>
+          </h2>
+          <ul className="space-y-2">
+            <li>
+              <p>日記を投稿します(毎日投稿とは限らない)。</p>
+            </li>
+            <li>
+              <p>Notion APIの仕様が許す限りデコります。</p>
+            </li>
+            <li>
+              <p>１週間に１回は投稿します。</p>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h2 className="text-2xl font-bold mb-4">
+            <Highlighter action="underline" color="#0000ff" multiline={false}>
+              やらないこと
+            </Highlighter>
+          </h2>
+          <ul className="space-y-2">
+            <li>
+              <p>このページのような構造化された形式では書きません。</p>
+            </li>
+            <li>
+              <p>毎日投稿はしません。</p>
+            </li>
+            <li>
+              <p>各投稿間の連続性は意識しません。</p>
+            </li>
+          </ul>
+        </section>
+      </div>
     </main>
   );
 }
